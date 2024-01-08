a7536f1bd080f03bca6d434f1f14266dfe1f9ee3
chowdarybvsn <chowdarybvsn@gmail.com>
2024-01-09 00:49:35 +0530

    local

--- terraform-base/provider.tf
+++ terraform-base/provider.tf
@@ -1,5 +1,8 @@
 terraform {
   #required_version = "0.12.14"
+  backend "local" {
+    path = "~/Documents/s3/S3/terraform-base/"
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
