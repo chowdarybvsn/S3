a42678a0488d9e41c85fc4b0f2d8a9149827e137
chowdarybvsn <chowdarybvsn@gmail.com>
2024-01-08 23:02:10 +0530

    yes

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-project-1-2023"
-    key    = "S3-bucket/backend.tfstate"
+    key    = "S3-dump/backend.tfstate"
     region = "us-east-1"
     dynamodb_table = "dynamodb-2024"
   }
